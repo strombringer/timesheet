9b19af23e41ce1d267c94f463faf9310e556eb1b
Matthias Schippling <matthias@schippling.dev>
2024-04-29 21:04:36 +0200

    fixed regex to work with days that were manually changed
    when the user forgot to check in/out with the card

--- process_timesheet.py
+++ process_timesheet.py
@@ -5,12 +5,12 @@ import fileinput
 
 # regular expressions
 regex_timeframe = re.compile(r'(\d{2}.\d{2}.\d{4}) bis (\d{2}.\d{2}.\d{4})')
-regexWfH = re.compile(r'ganz.+Mobilarbeit\s+([\d.,]+)')
-regexWfHpartial = re.compile(r'anteilige Mobilarbeit:\s+([\d.,]+)')
-regexWfO = re.compile(r'\d{2} \w{2}\s+\d{2}:\d{2}\s+\d{2}:\d{2}\s+[\d.,]+\s+([\d.,]+)')
+regexWfH = re.compile(r'ganz.+Mobilarbeit\s+(?P<actualWorkTime>[\d.,]+)')
+regexWfHpartial = re.compile(r'anteilige Mobilarbeit:\s+(?P<actualWorkTime>[\d.,]+)')
+regexWfO = re.compile(r'(?P<dayOfMonth>\d{2}) (?P<dayOfWeek>\w{2}).*(?P<startTime>\d{2}:\d{2}).*(?P<endTime>\d{2}:\d{2}).*(?P<break>[\d.,]{4,5}).*(?P<actualWorkTime>[\d.,]{4,5}).*(?P<expectedWorkTime>[\d.,]{4,5}).*(?P<overTime>[\d.,]{4,5})')
 
-def get_hours_worked(groups):
-    return float(groups[0].replace(',', '.'))
+def get_hours_worked(match):
+    return float(match.group('actualWorkTime').replace(',', '.'))
 
 # variables to store the results
 timeframe = ""
@@ -23,11 +23,11 @@ for line in fileinput.input():
     if (match := regex_timeframe.search(line)):
         timeframe = match.groups()[0] + "-" + match.groups()[1]
     elif (match := regexWfH.search(line)):
-        resultWfH += get_hours_worked(match.groups())
+        resultWfH += get_hours_worked(match)
     elif (match := regexWfO.search(line)):
-        resultWfO += get_hours_worked(match.groups())
+        resultWfO += get_hours_worked(match)
     elif (match := regexWfHpartial.search(line)):
-        resultWfH += get_hours_worked(match.groups())
+        resultWfH += get_hours_worked(match)
     # else:
     #     print("No match found for line:", line)
 
